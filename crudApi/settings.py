4e2c13e02f34822d0704a392e9d2386fe10634cf
rawheel <rawheeelalisiddiqui@gmail.com>
2021-03-14 19:43:26 +0500

    CRUD app Done!

--- crudApi/settings.py
+++ crudApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '%^8046nolr3(rlxhrcwdgkh((=@0i(00ox$go(&luuu&-sb$cj'
+SECRET_KEY = '%^8046nolr3(rlxhrcwdgkh((=@0i(00ox$go(&luuu&-sb$cj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
