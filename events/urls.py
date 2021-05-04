007c3dc81e4e558c51993b9185035bb8e13da9e3
rawheel <rawheeelalisiddiqui@gmail.com>
2021-05-05 01:57:52 +0500

    updated urls

--- events/urls.py
+++ events/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    #path('events/',views.EventList.as_view()),
+
     path('events/', views.EventList.as_view()),
     path('events/<int:pk>/', views.EventDetails.as_view()),
-    #path('eventdel/<int:pk>/', views.EventDel.as_view()),
+   
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
