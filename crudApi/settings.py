61eb97c451b77de287d4f7f77dd2b7ad850fc288
rawheel <rawheeelalisiddiqui@gmail.com>
2021-05-07 01:27:41 +0500

    added static

--- crudApi/settings.py
+++ crudApi/settings.py
@@ -138,5 +138,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
